0ea712fd6ec3036fca7948dde97e9c29f39c3eff
Pablo Leao <devpablol@gmail.com>
2024-04-21 23:40:07 -0300

    Update index.js

--- LogicOfProgramming/desafios/heroLevel/index.js
+++ LogicOfProgramming/desafios/heroLevel/index.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 
 let hero = [];
 
-rl.question('Digite o nome do herói: ', (name) => {
+rl.question('\nDigite o nome do herói: ', (name) => {
   hero.push(name);
   rl.question('Digite quanto de experiência o herói possui: ', (xp) => {
     hero.push(xp);
@@ -15,25 +15,25 @@ rl.question('Digite o nome do herói: ', (name) => {
 
     switch (true) {
       case convertXP <= 1000:
-        console.log(`O Herói de nome ${hero[0]} está no nível Ferro.`);
+        console.log(`\nO Herói de nome ${hero[0]} está no nível Ferro.`);
         break;
       case convertXP > 1000 && convertXP <= 5000 :
-        console.log(`O Herói de nome ${hero[0]} está no nível Bronze.`);
+        console.log(`\nO Herói de nome ${hero[0]} está no nível Bronze.`);
         break;
       case convertXP > 5000 && convertXP <= 6000:
-        console.log(`O Herói de nome ${hero[0]} está no nível Prata.`);
+        console.log(`\nO Herói de nome ${hero[0]} está no nível Prata.`);
         break;
       case convertXP > 6000 && convertXP <= 7000:
-        console.log(`O Herói de nome ${hero[0]} está no nível Ouro.`);
+        console.log(`\nO Herói de nome ${hero[0]} está no nível Ouro.`);
         break;
       case convertXP > 7000 && convertXP <= 8000:
-        console.log(`O Herói de nome ${hero[0]} está no nível Platina.`);
+        console.log(`\nO Herói de nome ${hero[0]} está no nível Platina.`);
         break;
       case convertXP > 8000 && convertXP <= 9000:
-        console.log(`O Herói de nome ${hero[0]} está no nível Ascendente.`);
+        console.log(`\nO Herói de nome ${hero[0]} está no nível Ascendente.`);
         break;
       case convertXP > 9000 && convertXP <= 10000:
-        console.log(`O Herói de nome ${hero[0]} está no nível Imortal.`);
+        console.log(`\nO Herói de nome ${hero[0]} está no nível Imortal.`);
         break;
       case convertXP > 10000: // maior que 10000
         console.log(`\nO Herói de nome ${hero[0]} está no nível Radiante.`);
