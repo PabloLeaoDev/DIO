a71b09a58123adb6f477ca2be626cf0a8fa3c7c9
Pablo Leão Dias dos Santos <devpablol@gmail.com>
2024-05-02 17:40:38 -0300

    Create ex4.js

---
+++ JavaScriptTraining/exercicios/ex4.js
@@ -0,0 +1,25 @@
+let preco = 100;
+let forma = 1;
+
+console.log('------------------------------------------');
+console.log('| Formas de Pagamento:                   |');
+console.log('|  1. Débito                             |');
+console.log('|  2. Dinheiro ou PIX                    |');
+console.log('|  3. Parcelado em duas vezes sem juros |');
+console.log('|  4. Parcelado em três vezes ou mais   |');
+console.log('------------------------------------------');
+
+switch (forma) {
+    case 1: // Débito
+        console.log(`Você recebeu 10% de desconto: o valor final do produto é R$${preco * 0.9}.`);
+        break;
+    case 2: // Dinheiro ou PIX
+        console.log(`Você recebeu 15% de desconto: o valor final do produto é R$${preco * 0.85}.`);
+        break;
+    case 3: // Parcelado em duas vezes sem juros
+        console.log(`O valor final do produto é R$${preco}.`);
+        break;
+    case 4: // Parcelado em três vezes ou mais
+        console.log(`Devido ao juros de 10%, o valor final do produto é R$${preco * 1.1}.`);
+        break;
+}
