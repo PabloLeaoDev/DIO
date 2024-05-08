7c7e2673dc683883a1112b22ab64f53c0131c9ca
Pablo Leão Dias dos Santos <devpablol@gmail.com>
2024-05-08 13:57:00 -0300

    Update ex8.js

--- JavaScriptTraining/exercicios/ex8.js
+++ JavaScriptTraining/exercicios/ex8.js
@@ -13,28 +13,29 @@ class Pessoa {
         return (this.peso / (this.altura ** 2)).toFixed(2);
     }
 
+    avaliarImc() {
+        const imc = this.calcularImc();
+        if (imc < 18.5) {
+            return 'Abaixo do peso ideal!';
+        } else if (imc >= 18.5 && imc <= 25) {
+            return 'Peso ideal.';
+        } else if (imc > 25 && imc <= 30) {
+            return 'Acima do peso...';
+        } else if (imc > 30 && imc <= 40) {
+            return 'Obeso!';
+        } else {
+            return 'Obesidade Mórbida!!';
+        }
+    }
+
     exibir() {
-        return `O valor do IMC de ${this.nome} é ${this.calcularImc()}. Ele está no nível: ${avaliarImc(this.calcularImc())}`;
+        return `O valor do IMC de ${this.nome} é ${this.calcularImc()}. Ele está no nível: ${this.avaliarImc()}`;
     }
 }
 
 const p1 = new Pessoa('José', '70', '1.75');
-const p2 = new Pessoa('Jonas', '110', '1.68');
+const p2 = new Pessoa('Jumba', '110', '1.68');
 
-function avaliarImc(imc) {
-    if (imc < 18.5) {
-        return 'Abaixo do peso ideal!';
-    } else if (imc >= 18.5 && imc <= 25) {
-        return 'Peso ideal.';
-    } else if (imc > 25 && imc <= 30) {
-        return 'Acima do peso...';
-    } else if (imc > 30 && imc <= 40) {
-        return 'Obeso!';
-    } else {
-        return 'Obesidade Mórbida!!';
-    }
-}
-
-// pode-se utilizar as variáveis e/ou funções globais em um método no JS. Mas não é possível criar varáveis locais no escopo dos métodos.
+// pode-se utilizar as variáveis e/ou funções globais em um método no JS.
 console.log(p1.exibir());
 console.log(p2.exibir());
