3193496fdc1c9e6d3dd7bc92b0205ad1d0cfb01a
Pablo Leão Dias dos Santos <devpablol@gmail.com>
2024-05-12 14:34:07 -0300

    Update ex13.js
    Código melhorado, versão final.

--- JavaScriptTraining/exercicios/ex13.js
+++ JavaScriptTraining/exercicios/ex13.js
@@ -1,15 +1,15 @@
 const {gets, list} = require('../gets-print/main');
 
-let maiorPar = 0;
-let menorImpar = 0;
+let maiorPar = null;
+let menorImpar = null;
 
 for (let index = 0; index < list.length; index++) {
     let num = list[index];
     const isPair = num % 2 === 0;
     const isOdd = num % 2 === 1;
-    if (isPair && num > maiorPar) {
+    if (isPair && (num > maiorPar || maiorPar === null)) {
         maiorPar = num;
-    } else if (isOdd && num < menorImpar || menorImpar === 0) {
+    } else if (isOdd && (num < menorImpar || menorImpar === null)) {
         menorImpar = num;
     }
 }
