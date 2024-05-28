fff3faa8fa16d4d3c934623baa21494f99eb7075
Pablo Leão Dias dos Santos <devpablol@gmail.com>
2024-05-28 09:32:30 -0300

    shpee-cart 1.0.1v finalizado

--- NodeJs/shopee-cart/src/index.js
+++ NodeJs/shopee-cart/src/index.js
@@ -6,11 +6,19 @@ let myWishList = []
 
 console.log('Welcome to the your Shopee Cart!')
 
-const item1 = await createItem('HotWheels Ferrari', 20.99, 1)
-const item2 = await createItem('HotWheels Lamborghini', 34.99, 3)
+const item1 = await createItem('HotWheels Ferrari', 20.99, 2)
+const item2 = await createItem('HotWheels Lamborghini', 34.99, 4)
+const item3 = await createItem('Dumbbells', 151, 2)
+const item4 = await createItem('PC Gamer', 2600, 1)
+
 await cartService.addItem(myCart, item1)
 await cartService.addItem(myCart, item2)
+await cartService.addItem(myWishList, item3)
+await cartService.addItem(myWishList, item4)
+
 await cartService.remItem(myCart, item2.name)
 await cartService.displayCart(myCart)
-// await cartService.delItem(myCart, item2.name)
+await cartService.delItem(myWishList, item3.name)
 cartService.calcTotal(myCart)
+await cartService.displayCart(myWishList)
+cartService.calcTotal(myWishList)
