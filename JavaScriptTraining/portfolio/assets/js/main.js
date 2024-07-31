14d1ec0f0b3a56f0443bf14b943661d7f8058cc0
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2024-07-31 20:22:07 -0300

    portfolio dio atualizado

--- JavaScriptTraining/portfolio/assets/js/main.js
+++ JavaScriptTraining/portfolio/assets/js/main.js
@@ -1,4 +1,28 @@
+
+function updateProfileInfo(profileData) {
+    const photo = document.querySelector('#profile-photo');
+    photo.src = profileData.photo;
+    photo.alt = profileData.name;
+    
+    const name = document.querySelector('#profile_name');
+    name.innerHTML = profileData.name;
+    
+    const job = document.querySelector('#profile_job');
+    job.innerHTML = profileData.job;
+    
+    const location = document.querySelector('#profile_location');
+    location.innerHTML = profileData.location;
+    
+    const phone = document.querySelector('#profile_phone');
+    phone.innerHTML = profileData.phone;
+    phone.href = `tel: ${profileData.phone}`;
+
+    const email = document.querySelector('#profile_email');
+    email.innerHTML = profileData.email;
+    email.href = `mailto: ${profileData.email}`;
+}
+
 (async () => {
     const profileData = await fetchProfileData();
-    console.log(profileData);
+    updateProfileInfo(profileData);
 })();
